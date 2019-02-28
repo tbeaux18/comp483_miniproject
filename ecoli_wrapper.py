165b9eec2395e0a944ee0772c67eae58eb4bb145
tbeaux18 <tbeaux18@gmail.com>
2019-02-28 17:44:58 -0600

    end to end testing

--- ecoli_wrapper.py
+++ ecoli_wrapper.py
@@ -434,8 +434,7 @@ def run_cufflinks_suite(gff_list, sorted_bam_list, assembly_file, merged_gtf, th
     """
 
     # array of top level directory for cufflink output files
-    # cuff_out_list = ['hm27_cuff', 'hm46_cuff', 'hm65_cuff', 'hm69_cuff']
-    cuff_out_list = ['hm27_cuff', 'hm46_cuff', 'hm65_cuff']
+    cuff_out_list = ['hm27_cuff', 'hm46_cuff', 'hm65_cuff', 'hm69_cuff']
 
     # building transcript assemblies with the sorted bam
     for gff_file, cuff_out, sorted_bam in zip(gff_list, cuff_out_list, sorted_bam_list):
@@ -458,15 +457,11 @@ def run_cufflinks_suite(gff_list, sorted_bam_list, assembly_file, merged_gtf, th
 
     # normalizing the merged transcriptome against each of its sorted bam
     LOGGER.info("Normalizing the merged transcriptome.")
-    cuffnorm_command = "cuffnorm -o diff_results -p {} {} {} {} {}".format(threads, merged_gtf, \
+    cuffnorm_command = "cuffnorm -o diff_results -p {} {} {} {} {} {}".format(threads, merged_gtf, \
                                                                     sorted_bam_list[0], \
                                                                     sorted_bam_list[1], \
-                                                                    sorted_bam_list[2])
-    # cuffnorm_command = "cuffnorm -o diff_results -p {} {} {} {} {} {}".format(threads, merged_gtf, \
-    #                                                                 sorted_bam_list[0], \
-    #                                                                 sorted_bam_list[1], \
-    #                                                                 sorted_bam_list[2], \
-    #                                                                 sorted_bam_list[3])
+                                                                    sorted_bam_list[2], \
+                                                                    sorted_bam_list[3])
 
     subprocess.run(cuffnorm_command, shell=True)
     LOGGER.info("Normalization complete.")
@@ -508,43 +503,41 @@ def main():
                         (HM69_FASTQ_1, HM69_FASTQ_2)]
 
     # output from prokka software
-    #gff_list = [HM27_GFF_FILE, HM46_GFF_FILE, HM65_GFF_FILE, HM69_GFF_FILE]
-    gff_list = [HM27_GFF_FILE, HM46_GFF_FILE, HM65_GFF_FILE]
+    gff_list = [HM27_GFF_FILE, HM46_GFF_FILE, HM65_GFF_FILE, HM69_GFF_FILE]
 
     # output from tophat2
-    #bam_file_list = [HM27_BAM, HM46_BAM, HM65_BAM, HM69_BAM]
-    bam_file_list = [HM27_BAM, HM46_BAM, HM65_BAM]
+    bam_file_list = [HM27_BAM, HM46_BAM, HM65_BAM, HM69_BAM]
 
     # output from samtools sort and tophat2
     sorted_bam_list = [HM27_SORTED_BAM, HM46_SORTED_BAM, HM65_SORTED_BAM, HM69_SORTED_BAM]
 
     # grabbing ftp files from ncbi using wget method
-    # LOGGER.info("Beginning to find FTP files.")
-    # wget_gunzip_fasta(fasta_ftp_list)
+    LOGGER.info("Beginning to find FTP files.")
+    wget_gunzip_fasta(fasta_ftp_list)
 
     # Parsing the FASTA and counting number of contigs and base pairs > 1000 in length
-    # LOGGER.info("Parsing FASTA and writing to log file.")
-    # parse_seqio_fasta(fasta_file_list, log_file)
+    LOGGER.info("Parsing FASTA and writing to log file.")
+    parse_seqio_fasta(fasta_file_list, log_file)
 
     # NEED TO FIX THE DIRECTORY PATH FOR WRITING TO THE LOG FILE!!
 
     # fix prokka to use less cpus and add the -usegenus flag
-    # LOGGER.info("Starting gene annotation with Prokka")
-    # build_prokka(fasta_file_list, log_file)
+    LOGGER.info("Starting gene annotation with Prokka")
+    build_prokka(fasta_file_list, log_file)
 
     # LOGGER.info("Grabbing SRA files and converting to FASTQ")
     # prefetch_fastq_decomp()
 
-    # LOGGER.info("Beginning alignment process.")
-    # build_tophat_alignment(fasta_file_list, fastq_tuple_list, bam_file_list, \
-    #                                                     sorted_bam_list, threads)
+    LOGGER.info("Beginning alignment process.")
+    build_tophat_alignment(fasta_file_list, fastq_tuple_list, bam_file_list, \
+                                                        sorted_bam_list, threads)
 
     LOGGER.info("Creating assembly file for cuffmerge.")
     with open('ecoli_assemblies.txt', 'w') as assemble_file:
         assemble_file.write("./hm27_cuff/transcripts.gtf\n")
         assemble_file.write("./hm46_cuff/transcripts.gtf\n")
         assemble_file.write("./hm65_cuff/transcripts.gtf\n")
-        #assemble_file.write("./hm69_cuff/transcripts.gtf\n")
+        assemble_file.write("./hm69_cuff/transcripts.gtf\n")
 
     LOGGER.info("Beginning to run cufflinks")
     run_cufflinks_suite(gff_list, bam_file_list, 'ecoli_assemblies.txt', MERGED_GTF, threads)
