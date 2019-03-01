44d249ef1abe0778976f680d385bc0ec8fd3c5f5
tbeaux18 <tbeaux18@gmail.com>
2019-03-01 09:56:52 -0600

    moving input files

--- run_wrapper.py
+++ run_wrapper.py
@@ -38,8 +38,8 @@ def main():
     ftp_files = args.ftp_links
     sra_file = args.sra_file
 
-    os.rename(ftp_files, FIRST_LAST_PATH + '/' + ftp_files)
-    os.rename(sra_file, FIRST_LAST_PATH + '/' + sra_file)
+    os.rename('../'+ ftp_files, FIRST_LAST_PATH + '/' + ftp_files)
+    os.rename('../'+ sra_file, FIRST_LAST_PATH + '/' + sra_file)
 
     with open(ftp_files, 'r') as ftp_input:
         assembly_name_list = [line.strip().split(',')[0] for line in ftp_input]
